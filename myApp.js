bc811b0cd678d1a0912b9929e5ae2e3df6efb866
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-02-07 20:07:24 -0300

    [5] Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -13,6 +13,11 @@ app.get("/", (req, res) => {
   res.sendFile(absolutePath); // Respond with an HTML file
 });
 
+app.get("/json", (req, res) => {
+    // res.send('Hello Express');  Respond with "Hello Express" when a GET request is made to the homepage
+    res.json({"message": "Hello json"}); // Respond with an HTML file
+  });
+
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
   });

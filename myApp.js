02b2335d709dd29bde303509dab42fe7d3a0526b
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-02-07 20:40:24 -0300

    [6] Different approach. Change logic

--- myApp.js
+++ myApp.js
@@ -4,29 +4,23 @@ let app = express();
 const PORT = 3030;
 console.log("Hello World");
 
-style = process.env.MESSAGE_STYLE;
-message = "Hello json";
-absolutePath = __dirname + "/views/index.html";
+const defaultMessage = "Hello json";
+const style = process.env.MESSAGE_STYLE || 'uppercase';
+const message = (style === 'uppercase') ? defaultMessage.toUpperCase() : defaultMessage;
 
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  // res.send('Hello Express');  Respond with "Hello Express" when a GET request is made to the homepage
-  res.sendFile(absolutePath); // Respond with an HTML file
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req, res) => {
-    if (style === 'uppercase'){
-        res.json({"message": message.toUpperCase()}); 
-    }
-    else {
-        res.json({"message": message}); 
-    }
-  });
+    res.json({"message": message});
+});
 
 app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
-  });
+    console.log(`Server started on port ${PORT}`);
+});
 
 module.exports = app;

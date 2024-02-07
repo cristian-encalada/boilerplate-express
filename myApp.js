310f3ce1439259e8661ef9918594b2700405bc2f
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-02-07 18:30:28 -0300

    [2] Include basic app.get() method

--- myApp.js
+++ myApp.js
@@ -1,12 +1,10 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-
-
-
-
-
-
+// Respond with "Hello Express" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+  })
 
 
 
